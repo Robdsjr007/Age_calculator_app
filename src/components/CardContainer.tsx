8206cf7cc5a5390462fd263144c38ad1410f908a
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-10-01 01:20:00 -0300

    iniciando o challenge

---
+++ src/components/CardContainer.tsx
@@ -0,0 +1,11 @@
+import styles from './CardContainer.module.sass';
+
+const CardContainer = () => {
+  return (
+    <article className={styles.card}>
+
+    </article>
+  )
+}
+
+export default CardContainer
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.sass'
+
+const Home = () => {
+  return (
+    <main className={styles.container}>
+        
+    </main>
+  )
+}
+
+export default Home
